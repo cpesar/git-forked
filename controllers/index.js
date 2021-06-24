67c3d1c0577dd5cb31e034aa6460e15f3f389989
Chewie <chewie1985@gmail.com>
2021-06-23 20:45:27 -0600

    restaurants

--- controllers/index.js
+++ controllers/index.js
@@ -5,9 +5,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const dishboardRoutes = require('./dishboard-routes');
 const homepageRoutes = require('./homepage-routes');
+const restRoutes = require('./rest-routes');
 
 router.use('/', dishboardRoutes);
 router.use('/', homepageRoutes);
 router.use('/api', apiRoutes);
+router.use('/restaurants', restRoutes);
 
 module.exports = router;
