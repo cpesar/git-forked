3c92fe564739bba9a75fb93f453054ab7af9a768
Shawn Cochran <shawn.cochran31@gmail.com>
2021-06-27 19:49:01 -0600

    communication on web console

--- controllers/rest-routes.js
+++ controllers/rest-routes.js
@@ -8,39 +8,29 @@ router.get("/", (req, res) => {
   // }
 });
 
-router.get("/restaurant", (req, res) => {
+router.post("/restaurant", (req, res) => {
   let API_KEY = process.env.DB_API_KEY;
   console.log(API_KEY, "API KEY:")
   let zipcode = req.body.zipcode;
   let rating = req.body.rating;
   let price = req.body.price;
   let cuisine = req.body.cuisine;
-  
 
-    //   var config = {
-    //     method: "get",
-    //     url: `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${zipcode}&rating=${rating}&price=${price}&categories=${cuisine}&limit=10`,
-    //     headers: {
-    //       Authorization: `Bearer ${API_KEY}`,
-    //     },
-    //   };
-
-    //   axios(config)
-    //     .then(function (response) {
-    //       return res.json(response.data);
-    //     })
-    //     .catch(function (error) {
-    //       console.log(error);
-    //     });
-
-    ; (async () => {
-      const response = await axios({
+      var config = {
+        method: "get",
         url: `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${zipcode}&rating=${rating}&price=${price}&categories=${cuisine}&limit=10`,
-        method: 'get'
-      })
+        headers: {
+          Authorization: `Bearer ${API_KEY}`,
+        },
+      };
 
-      console.log(response.body);
-    })()
+      axios(config)
+        .then(function (response) {
+          return res.json(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
 });
 
 
--- public/js/restaurants.js
+++ public/js/restaurants.js
@@ -7,30 +7,21 @@ async function restaurantFormHandler(event) {
     const zipcode = document.querySelector("#zipcode").value.trim();
 
     if (cuisine || rating || price || zipcode) {
-        // let response = await fetch("/restaurants", {
-        //     method: "GET",
-        //     headers: { "Content-Type": "application/json" },
-        //     params: {
-        //         cuisine: `${cuisine}`,
-        //         rating: `${rating}`,
-        //         price: `${price}`,
-        //         zipcode: `${zipcode}`
-        //     }
-        // });
-
-        // if (response.ok) {
-        //     console.log(response.params);
-        // } else {
-        //     alert("Please select all options.");
-        // }
-        ; (async () => {
-            const response = await axios({
-              url: `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurants&location=${zipcode}&rating=${rating}&price=${price}&categories=${cuisine}&limit=10`,
-              method: 'get'
-            })
-      
-            console.log(response.body);
-          })()
+        let response = await fetch("/restaurants", {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+            params: {
+                cuisine: `${cuisine}`,
+                rating: `${rating}`,
+                price: `${price}`,
+                zipcode: `${zipcode}`
+            },
+        })
+        if (response.ok) {
+            console.log(response);
+        } else {
+            alert("Please select all options.");
+        }
     }
 }
 
